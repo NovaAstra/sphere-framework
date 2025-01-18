abc30a5744c8190ca8055b0cb2e5bd462114fa37
NovaAstra <NovaAstra@outlook.com>
2025-01-18 15:29:01 +0800

    main

--- configs/eslint/eslint-config/src/create-config.ts
+++ configs/eslint/eslint-config/src/create-config.ts
@@ -1,29 +1,28 @@
-import type { Promisable, Arrayable } from "@sphere/typing"
+import type { Arrayable, Promisable } from "@sphere/typing"
 import type { ESLintConfig } from "./typings"
 
-export type CreateConfigFunction<
-  O = void,
-  E extends ESLintConfig = ESLintConfig> =
-  O extends void
-  ? () => Arrayable<E>
-  : (options?: O) => Promisable<Arrayable<E>>
+export function createConfig<O = void, E extends ESLintConfig = ESLintConfig>(
+  config: (options: O) => ESLintConfig
+): (options: O) => E
+export function createConfig<O = void, E extends ESLintConfig = ESLintConfig>(
+  config: (options: O) => ESLintConfig[]
+): (options: O) => E[]
 
-export function createConfig<
-  E extends ESLintConfig,
-  T extends Arrayable<E>,
-  F extends CreateConfigFunction<void, ESLintConfig>
->(config: T): () => (T extends ESLintConfig[] ? E[] : E)
+export function createConfig<O = void, E extends ESLintConfig = ESLintConfig>(
+  config: (options: O) => Promise<ESLintConfig>
+): (options: O) => Promise<E>
+export function createConfig<O = void, E extends ESLintConfig = ESLintConfig>(
+  config: (options: O) => Promise<ESLintConfig[]>
+): (options: O) => Promise<E[]>
 
+export function createConfig<E extends ESLintConfig = ESLintConfig>(config: ESLintConfig): () => E
+export function createConfig<E extends ESLintConfig = ESLintConfig>(config: ESLintConfig[]): () => E[]
 
-export function createConfig(config: any) {
+export function createConfig<O = void>(
+  config: Arrayable<ESLintConfig>
+    | ((options: O) => Promisable<Arrayable<ESLintConfig>>)
+) {
   return typeof config === 'function'
     ? config
     : () => config
 }
-
-const a = createConfig({})
-const b = createConfig([])
-// const c = createConfig(() => ({}))
-// const d = createConfig(() => ([]))
-// const e = createConfig(async () => ({}))
-// const f = createConfig(async () => ([])) 
