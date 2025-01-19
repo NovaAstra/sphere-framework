b8605ed1086e9a2fbdabed8503a655c448872387
NovaAstra <NovaAstra@outlook.com>
2025-01-19 13:55:09 +0800

    main

--- configs/eslint/eslint-config/src/define-config.ts
+++ configs/eslint/eslint-config/src/define-config.ts
@@ -1,17 +1,4 @@
-import type { Arrayable } from "@sphere/typing"
-import type { ESLintConfig } from "./typings"
-import { typescript } from "./typescript"
-
-export interface ESLintOptions {
-  vue?: boolean;
-  jsx?: boolean;
-  gitignore?: boolean;
-}
 
 export function defineConfig() {
-  const configs: Arrayable<ESLintConfig> = [];
-
-  configs.push(...(typescript({ typescript: true })) as ESLintConfig[])
-
-  return configs;
+ 
 }
--- configs/eslint/eslint-config/src/ignore/index.ts
+++
@@ -1,8 +0,0 @@
-import { createConfig } from "../create-config"
-import { ESLintConfig } from "../typings";
-
-export const ignore = createConfig(() => {
-  const configs: ESLintConfig[] = []
-
-  return configs;
-})
