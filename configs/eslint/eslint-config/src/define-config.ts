e1756fc73d7a5da9a6fb919eb838f592fdb2b341
NovaAstra <NovaAstra@outlook.com>
2025-01-19 14:23:55 +0800

    main

--- configs/eslint/eslint-config/src/define-config.ts
+++ configs/eslint/eslint-config/src/define-config.ts
@@ -1,4 +1,20 @@
+import type { Arrayable } from "@sphere/typing"
+import type { ESLintConfig } from "./typings"
 
-export function defineConfig() {
- 
+import { toArray } from "@sphere/shared"
+
+export interface ESLintOptions {
+
+}
+
+export async function defineConfig(
+  config: Arrayable<ESLintConfig> = [],
+  options: ESLintOptions = {}
+) {
+  const configs: ESLintConfig[] = []
+
+  if (Object.keys(config).length > 0)
+    configs.push(...toArray(config))
+
+  return configs;
 }
---
+++ configs/eslint/eslint-config/src/typings.ts
@@ -0,0 +1,3 @@
+import type { FlatESLintConfig } from 'eslint-define-config'
+
+export type ESLintConfig = FlatESLintConfig
